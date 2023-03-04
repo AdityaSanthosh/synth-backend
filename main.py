399f089a44f020c3058cf5ad844be70d3f02e09e
AdityaSanthosh <pssanthoshaditya2000@gmail.com>
2023-03-05 01:13:24 +0530

    setup the comms to frontend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from supabase import create_client, Client
 from pydantic import BaseModel
+import email
+from bs4 import BeautifulSoup
 
 app = FastAPI()
 
@@ -9,18 +11,28 @@ key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI
 supabase: Client = create_client(url, key)
 
 
-# class PageData(BaseModel):
-#     user_id: str
-#     webpage: str
-#     page_content:
-
 @app.get("/")
 async def root():
     return {"message": "Hello Synth"}
 
 
-# @app.post("/insert")
-# async def putData()
+@app.post("/savepage")
+async def putData(file: UploadFile = File()):
+    raw_string = file.file.read().decode()
+    with open("temp.txt","w") as f:
+        f.write(raw_string)
+    with open("temp.txt","r") as fp:
+        message = email.message_from_file(fp)
+        for part in message.walk():
+            if part.get_content_type() == "text/html":
+                with open("page.html","w") as f:
+                    f.write(part.get_payload(decode=False))
+    with open("page.html") as fp:
+        soup = BeautifulSoup(fp, 'lxml')
+    p_content = soup.find_all('p')
+    print(p_content[20].text)
+    # remove unnecessary symbols from text
+    return {"file_size": "len(file)"}
 
 
 @app.get("/search")
